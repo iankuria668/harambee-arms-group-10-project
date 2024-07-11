e3618cf3bf9e0cf7ec7514e315c87e3afc1146d8
Sharon Byegon <byegon.sharon@gmail.com>
2024-07-11 10:07:54 +0300

    Configured Flask application

--- server/app.py
+++ server/app.py
@@ -3,20 +3,23 @@
 # Standard library imports
 
 # Remote library imports
-from flask import request
+from flask import request, session, make_response
 from flask_restful import Resource
 
 # Local imports
 from config import app, db, api
+
 # Add your model imports
+
 
 
 # Views go here!
 
-@app.route('/')
-def index():
-    return '<h1>Project Server</h1>'
-
+class Home(Resource):
+    def get(self):
+        return '<h1> Phase 4 Project Server </h1>'
+    
+api.add_resource(Home, '/')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
