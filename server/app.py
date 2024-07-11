bac3848911084dabb5671931051aa080610aef3e
Sharon Byegon <byegon.sharon@gmail.com>
2024-07-11 15:55:26 +0300

    Completed CRUD application for Class Order

--- server/app.py
+++ server/app.py
@@ -12,9 +12,6 @@ from config import app, db, api
 # Add your model imports
 from models import Customer, Item, Order, OrderItem
 
-
-
-
 # Views go here!
 
 class Home(Resource):
@@ -71,6 +68,43 @@ class ItemsByID(Resource):
             db.session.commit()
 
             return make_response(item_to_update.to_dict(), 202)
+    
+    def delete(self, id):
+        item_to_delete = Item.query.filter(Item.id == id).first()
+        if item_to_delete:
+            db.session.delete(item_to_delete)
+            db.session.commit()
+
+            return make_response({'message': 'Item deleted'},200)
+        else:
+            return {'error': 'Item not found'}, 404
+        
+class Orders(Resource):
+    def get(self):
+        orders = [order.to_dict() for order in Order.query.all()]
+        return make_response(orders, 200)
+    
+    def post(self):
+        data = request.get_json()
+
+        new_order = Order(
+            customer_id = data['customer_id'],
+            total = data['total']
+        )
+        db.session.add(new_order)
+        db.session.commit()
+
+        return make_response(new_order.to_dict(), 201)
+    
+class OrdersByID(Resource):    
+    def delete(self, id):
+        order_to_delete = Order.query.filter(Order.id == id).first()
+        if order_to_delete:
+            db.session.delete(order_to_delete)
+            db.session.commit()
+            return make_response({'message': 'Order deleted'},200)
+        else:
+            return {'error': 'Order not found'}, 404
 
         
     
@@ -78,7 +112,8 @@ api.add_resource(Home, '/')
 api.add_resource(Items, '/items')
 api.add_resource(ItemsByCategory, '/items/<category>')
 api.add_resource(ItemsByID, '/items/<int:id>')
-
+api.add_resource(Orders, '/orders')
+api.add_resource(OrdersByID, '/orders/<int:id>')
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
 
