81b7d81f4a9890f19298fa8597661d81904587e5
iankuria668 <iankuria668@gmail.com>
2024-07-12 15:36:43 +0300

    Router configuration

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,17 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import App from "./components/App";
+import { BrowserRouter } from "react-router-dom";
+import { createRoot } from "react-dom/client";
 import "./index.css";
 import { createRoot } from "react-dom/client";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(<App />);
+
+root.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+    );
