7d6e37e8bd31974f17acb9b73fd751c69b2d5b83
iankuria668 <iankuria668@gmail.com>
2024-07-12 16:19:44 +0300

    Debugging

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -10,7 +10,7 @@ function Home({ items, addToCart, removeFromCart, addToWishlist }){
     return(
         <div>
             <br />
-            <h2 className="intro" id='welcome' style={{fontSize: "50px"}}>Welcome to the Ye Olde Dragonmart!</h2>
+            <h2 className="intro" id='welcome' style={{fontSize: "50px"}}>Welcome to Harambee Arms!</h2>
             <p className="intro" id='greeting'> Shop weapons, armor, tools, and magic items til your pockets run dry. You'll never find a better deal in all of Brittania!</p>
             <br />
             <h3 className="intro" id='featureTitle'>Featured Items:</h3>
