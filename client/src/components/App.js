2809281b300310732be5b29c4d4e8a7cde892210
Sharon Byegon <byegon.sharon@gmail.com>
2024-07-17 07:47:26 +0300

    Added Login and signup to app.js component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,73 +1,90 @@
 import React, { useEffect, useState } from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import NavBar from './Navbar';
 import Account from './Account';
 import Home from './Home';
 import Shop from './Shop';
 import Cart from './Cart';
 import Wishlist from './Wishlist';
+import Signup from './Signup';
+import Login from './Login';
 import './App.css';
 import '../index.css';
 
 function App() {
-  const [page, setPage] = useState('/')
-  const [items, setItems] = useState([])
-  const [wallet, setWallet] = useState(0)
-  const [inCart, setInCart] = useState([])
+  const [items, setItems] = useState([]);
+  const [inCart, setInCart] = useState([]);
   const [inWishlist, setInWishlist] = useState([]);
-
-  function addToCart(addedItem){
-    const cartItems = inCart.find(
-      (item) => item.id === addedItem.id
-    );
-
-    if (!cartItems){
-      setInCart([...inCart, addedItem]);
-    }
-  }
-
-  function removeFromCart(removedItem){
-    const editedCart = inCart.filter(item => item.title !== removedItem.title);
-    setInCart(editedCart);
-  }
-
-  function addToWishlist(wishItem) {
-    const wished = inWishlist.find(
-      (item) => item.id === wishItem.id
-    );
-
-    if (!wished){
-      setInWishlist([...inWishlist, wishItem])
-    }
-    alert('Added to wishlist')
-  }
-
-  function removeFromWishlist(removeWish){
-    const editedWishlist = inWishlist.filter(item => item.title !== removeWish.title);
-    setInWishlist(editedWishlist);
-  }
-  
+  const [wallet, setWallet] = useState(0);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const navigate = useNavigate();
 
   useEffect(() => {
     fetch('http://127.0.0.1:5555/items')
-    .then(res => res.json())
-    .then(data => {
-      setItems(data)
-    });
-  }, [items.length += 1]);
+      .then(res => res.json())
+      .then(data => setItems(data));
+  }, []);
+
+  useEffect(() => {
+    const token = localStorage.getItem('jwt');
+    if (token) {
+      setIsAuthenticated(true);
+    } else {
+      setIsAuthenticated(false);
+    }
+  }, []);
+
+  const addToCart = (addedItem) => {
+    const cartItems = inCart.find((item) => item.id === addedItem.id);
+    if (!cartItems) {
+      setInCart([...inCart, addedItem]);
+    }
+  };
+
+  const removeFromCart = (removedItem) => {
+    const editedCart = inCart.filter(item => item.title !== removedItem.title);
+    setInCart(editedCart);
+  };
+
+  const addToWishlist = (wishItem) => {
+    const wished = inWishlist.find((item) => item.id === wishItem.id);
+    if (!wished) {
+      setInWishlist([...inWishlist, wishItem]);
+    }
+    alert('Added to wishlist');
+  };
+
+  const removeFromWishlist = (removeWish) => {
+    const editedWishlist = inWishlist.filter(item => item.title !== removeWish.title);
+    setInWishlist(editedWishlist);
+  };
+
+  const handleLogin = (data) => {
+    localStorage.setItem('jwt', data.access_token);
+    setIsAuthenticated(true);
+    navigate('/');
+  };
+
+  const handleLogout = () => {
+    localStorage.removeItem('jwt');
+    setIsAuthenticated(false);
+    navigate('/login');
+  };
 
   return (
     <div className='App'>
-      <NavBar onChangePage={setPage}/>
+      {isAuthenticated && <NavBar onLogout={handleLogout} />}
       <Routes>
-        <Route path='/shop' element={<Shop items={items} addToCart={addToCart}  addToWishlist={addToWishlist}/>}></Route>
-        <Route path='/wishlist' element={<Wishlist inWishlist={inWishlist} setInWishlist={setInWishlist} removeFromWishlist={removeFromWishlist} addToCart={addToCart}/>}></Route>
-        <Route path='/cart' element={<Cart inCart={inCart} setInCart={setInCart} removeFromCart={removeFromCart} wallet={wallet} setWallet={setWallet}/>}></Route>
-        <Route path='/account' element={<Account wallet={wallet} setWallet={setWallet} items={items}/>}></Route>
-        <Route path='/' element={<Home items={items} addToCart={addToCart} removeFromCart={removeFromCart} addToWishlist={addToWishlist}/>}></Route>
+        <Route path='/shop' element={<Shop items={items} addToCart={addToCart} addToWishlist={addToWishlist} />} />
+        <Route path='/wishlist' element={<Wishlist inWishlist={inWishlist} setInWishlist={setInWishlist} removeFromWishlist={removeFromWishlist} addToCart={addToCart} />} />
+        <Route path='/cart' element={<Cart inCart={inCart} setInCart={setInCart} removeFromCart={removeFromCart} wallet={wallet} setWallet={setWallet} />} />
+        <Route path='/account' element={<Account wallet={wallet} setWallet={setWallet} items={items} />} />
+        <Route path='/' element={<Home items={items} addToCart={addToCart} removeFromCart={removeFromCart} addToWishlist={addToWishlist} />} />
+        <Route path='/signup' element={<Signup onSignup={handleLogin} />} />
+        <Route path='/login' element={<Login onLogin={handleLogin} />} />
       </Routes>
     </div>
-  )
+  );
 }
 
 export default App;
