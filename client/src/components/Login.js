347d2e4d49c89389a5ea4bc2926b45d588da1e6a
Sharon Byegon <byegon.sharon@gmail.com>
2024-07-18 06:58:45 +0300

    Styled the login page

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
+import './Login.css'
 
 function Login({ onLogin }) {
     const [username, setUsername] = useState('');
